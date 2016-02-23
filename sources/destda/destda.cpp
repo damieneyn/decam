cda81d8144ea6063c9878443a9074c1b1384208f
deynard <d.eynard@py-innovation.com>
2016-02-23 16:11:02 +0100

    Update standalone exec and sample to test camera

--- sources/destda/destda.cpp
+++ sources/destda/destda.cpp
@@ -1,9 +1,7 @@
-#include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
-#include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
-#include <opencv2/videoio/videoio.hpp>
-#include <opencv2/highgui/highgui.hpp>  // OpenCV window I/O
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
+#include "decam/dueye.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <stdio.h>
 #include <string>
@@ -12,10 +10,53 @@
 using namespace cv;
 using namespace std;
 
-
 int main(int argc, char* argv[])
 {
+	CVuEye cam;
+
+	/******************************/
+	/* Sample single shot grabber */
+	/******************************/
+	
+	/* OPENCV SAMPLE */
+	/*
+	IplImage *img = cam.getCvImage();
+	do
+	{
+		cam.GrabFrame();
+		//cam.SaveImage(L"toto.jpg");
+
+		//cam.saveCvImage("test", 0, ".jpg");
+		cvShowImage("image", img);
+	} while (cvWaitKey(1) != 'q');
+
+	cvDestroyWindow("image");
+	*/
+
+	/* OPENCV2 SAMPLE */
+
+	Mat img = cam.getMat();
+	do
+	{
+		cam.GrabFrame();
+		imshow("Image", img);
+
+	} while (waitKey(1) != 'q');
+
+	/**************************/					
+	/* Sample FreeRun grabber */
+	/**************************/
+	/*
+	Mat img = cam.getMat();
+
+	cam.StartLive();
+	do{
+		cam.getCvLastRingBuffer();
+		imshow("Image", img);
+	} while (waitKey(1) != 'q');
+	cam.StopLive();
+	*/
+
 
 	return 0;
 }
-
